f9f538570af07baae57bd1edd29f39847c555cdc
Alok kumar <herealok01@gmail.com>
2025-02-04 01:56:11 +0530

    scripttt.js

--- scripttt.js
+++ scripttt.js
@@ -1,7 +1,8 @@
 const exchangeRates = {
-    USD: { USD: 1, EUR: 0.91, GBP: 0.79 },
-    EUR: { USD: 1.10, EUR: 1, GBP: 0.87 },
-    GBP: { USD: 1.27, EUR: 1.15, GBP: 1 },
+    USD: { USD: 1, EUR: 0.91, POUND: 0.79, INR: 71.25 },
+    EUR: { USD: 1.10, EUR: 1, POUND: 0.87, INR: 78.23 },
+    POUND : { USD: 1.27, EUR: 1.15, POUND: 1, INR: 90.35 },
+    INR : { USD: 0.011, EUR: 0.011, POUND: 0.0093, INR: 1 },
   };
 
   function convertCurrency() {
